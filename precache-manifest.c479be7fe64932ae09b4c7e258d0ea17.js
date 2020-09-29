0e207c1392646b6948cf4322f97dd8dd82d97ad0
ArunTeltia <teltia.arun18@gmail.com>
2020-09-29 22:27:00 +0530

    Updates

---
+++ precache-manifest.c479be7fe64932ae09b4c7e258d0ea17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb122f04a17d36d14ce421baf3bf139",
+    "url": "/wiki/index.html"
+  },
+  {
+    "revision": "268c921e9a9827502089",
+    "url": "/wiki/static/css/main.12b16c64.chunk.css"
+  },
+  {
+    "revision": "a771960744e34f5a6e05",
+    "url": "/wiki/static/js/2.9831431c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wiki/static/js/2.9831431c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268c921e9a9827502089",
+    "url": "/wiki/static/js/main.7b731106.chunk.js"
+  },
+  {
+    "revision": "5f9a17241f50d750b7a7",
+    "url": "/wiki/static/js/runtime-main.7b210d1f.js"
+  }
+]);
